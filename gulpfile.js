81863bd24c718548833cb656b4a6ecd5beaf6396
sexfat <bryant6831@gmail.com>
2020-08-17 11:18:32 +0800

    add 同步異步

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,26 @@
 const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
 
 
 
-gulp.task('hi' ,function () {
-   //do thing
-   console.log('測試test');  
+gulp.task('hi', function () {
+    //do thing
+    console.log('測試test');
 });
 
-gulp.task('copy' , function(){
-   return gulp.src('dev/index.html').pipe(gulp.dest('dist'));
+gulp.task('copy', function () {
+    return gulp.src('dev/index.html').pipe(gulp.dest('dist'));
 })
+
+//異步
+gulp.task('css',['copy'] ,function () {
+    return gulp.src('dev/css/reset.css')
+    .pipe(cleanCSS({
+        compatibility: 'ie8'
+    })).pipe(gulp.dest('dist/css'))
+})
+
+//
+  gulp.task('all' ,['copy' ,'css']);//同步
+
+
