7e3c46663b6d743a8c18971a1e160bb1975cc4ed
sexfat <bryant6831@gmail.com>
2020-08-19 10:31:04 +0800

    add html

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const cleanCSS = require('gulp-clean-css');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
+const fileinclude = require('gulp-file-include');
 
 
 
@@ -15,41 +16,51 @@ gulp.task('copy', function () {
 })
 
 //異步
-gulp.task('css',['copy'] ,function () { //先執行 ['copy'] 這個function
+gulp.task('css', ['copy'], function () { //先執行 ['copy'] 這個function
     return gulp.src('dev/css/*.css')
-    .pipe(cleanCSS({
-        compatibility: 'ie8'
-    })).pipe(gulp.dest('dist/css'))
+        .pipe(cleanCSS({
+            compatibility: 'ie8'
+        })).pipe(gulp.dest('dist/css'))
 })
 
 //sass
 
 gulp.task('sass', function () {
     return gulp.src('./dev/sass/*.scss') //來源
-      .pipe(sass().on('error', sass.logError))//轉譯sass
-      .pipe(cleanCSS({
-        compatibility: 'ie8'
-    }))// 壓縮css
-      .pipe(gulp.dest('./dist/css')); //目的地
-  });
+        .pipe(sass().on('error', sass.logError)) //轉譯sass
+        .pipe(cleanCSS({
+            compatibility: 'ie8'
+        })) // 壓縮css
+        .pipe(gulp.dest('./dist/css')); //目的地
+});
 
-  gulp.task('concat', ['sass'], function () {
-    //do
+
+
+//打包
+gulp.task('concat', ['sass'], function () {
     return gulp.src('dist/css/*.css') //來源
-      .pipe(concat('all.css')) //合併
-      .pipe(gulp.dest('dist/css/all')) //目的地
-  })
+        .pipe(concat('all.css')) //合併
+        .pipe(gulp.dest('dist/css/all')) //目的地
+})
 
+
+//html 
+gulp.task('fileinclude', function () {
+    return gulp.src(['dev/*.html']) //來源
+      .pipe(fileinclude({
+        prefix: '@@',
+        basepath: '@file'
+      }))
+      .pipe(gulp.dest('./dist')); //目的地
+  });
 
 
 
 //監看變動
-gulp.task('watch' ,function(){
-   gulp.watch('./dev/sass/*.scss' ,['sass'])
+gulp.task('watch', function () {
+    gulp.watch('./dev/sass/*.scss', ['sass'])
 })
 
 
 //同步
-  gulp.task('all' ,['copy' ,'css']);//同時執行 ['copy' ,'css'] 這兩個function
-
-
+gulp.task('all', ['copy', 'css']); //同時執行 ['copy' ,'css'] 這兩個function
