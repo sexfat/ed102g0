e80a6a5a8898b88c4af1bf63f59ecb3790554cf2
sexfat <bryant6831@gmail.com>
2020-08-19 16:00:16 +0800

    add socurmap

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,10 @@ const cleanCSS = require('gulp-clean-css');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
 const fileinclude = require('gulp-file-include');
+const browserSync = require('browser-sync');
+const imagemin = require('gulp-imagemin');
+var sourcemaps = require('gulp-sourcemaps');
+const reload = browserSync.reload;
 
 
 
@@ -27,10 +31,12 @@ gulp.task('css', ['copy'], function () { //先執行 ['copy'] 這個function
 
 gulp.task('sass', function () {
     return gulp.src('./dev/sass/*.scss') //來源
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError)) //轉譯sass
         .pipe(cleanCSS({
             compatibility: 'ie8'
         })) // 壓縮css
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('./dist/css')); //目的地
 });
 
@@ -47,20 +53,42 @@ gulp.task('concat', ['sass'], function () {
 //html 
 gulp.task('fileinclude', function () {
     return gulp.src(['dev/*.html']) //來源
-      .pipe(fileinclude({
-        prefix: '@@',
-        basepath: '@file'
-      }))
-      .pipe(gulp.dest('./dist')); //目的地
-  });
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        .pipe(gulp.dest('./dist')); //目的地
+});
 
 
 
 //監看變動
 gulp.task('watch', function () {
-    gulp.watch('./dev/sass/*.scss', ['sass' , 'concat']);
-    gulp.watch(['dev/*.html' , 'dev/**/*.html'] , ['fileinclude']);
+    gulp.watch('./dev/sass/*.scss', ['sass', 'concat']);
+    gulp.watch(['dev/*.html', 'dev/**/*.html'], ['fileinclude']);
+});
+
+
+//壓圖
+gulp.task('img', function () {
+    gulp.src('./dev/images/*.*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
 })
+
+
+
+//瀏覽器同步
+gulp.task('default', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./dist",
+            index: "index.html"
+        }
+    });
+    gulp.watch('./dev/sass/*.scss', ['sass', 'concat']).on('change', reload);
+    gulp.watch(['dev/*.html', 'dev/**/*.html'], ['fileinclude']).on('change', reload);
+});
 
 
 //同步
