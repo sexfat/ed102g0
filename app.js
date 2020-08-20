b52e6fc69ce2fe70694dce1f820761a2c5fc9ffc
sexfat <bryant6831@gmail.com>
2020-08-20 15:05:43 +0800

    add css style

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
-const x = 10
+import  "./style.css";
+
+
+
 console.log('webpack4 start');
--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,24 @@ const path = require('path');
 
 module.exports = {
     entry: {
-       app : './app.js',
-       app2 : './app2.js'
+       app : './app.js'
     },               // 入口文件
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js'
       },             // 出口文件
-    //module: {},              // 處裡對應模組
+      module: {
+        rules: [{
+            // 格式
+            test: /\.css$/,
+            //順序是由下到上 css > style
+            use: [
+                'style-loader',
+                'css-loader'
+            ],
+        }]
+
+    },              // 處裡對應模組
     //plugins: [],             // 對應的插件
     //devServer: {},           // 服務器配置
     mode: 'development'       // 開發模式配置 development
